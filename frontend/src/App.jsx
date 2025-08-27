40b6e37f2185a08b43e9aeef81a84fe3b6ec9bbb
makhan singh <makhansingh79731@gmail.com>
2025-08-27 15:39:25 +0530

    admin oage changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/Newcar";
+// import About from "./pages/Newcar";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import Register from "./pages/Register";
@@ -10,10 +10,10 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import PageNotFound from "./pages/PageNotFound";
 import Logout from "./pages/Logout";
-import AdminPage from "./components/AdminLayout/AdminPage";
-import AdminUser from "./components/adminlayout/AdminUser";
-import AdminContact from "./components/adminlayout/AdminContact";
-import UpdateUser from "./components/adminlayout/AdminUpdate";
+// import AdminPage from "./components/AdminLayout/AdminPage";
+// import AdminUser from "./components/adminlayout/AdminUser";
+// import AdminContact from "./components/adminlayout/AdminContact";
+// import UpdateUser from "./components/adminlayout/AdminUpdate";
 import Addcar from "./pages/Addcar";
 import Newcar from "./pages/Newcar";
 
@@ -32,11 +32,11 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
           <Route path="*" element={<PageNotFound />} />
-          <Route path="/admin" element={<AdminPage />}>
+          {/* <Route path="/admin" element={<AdminPage />}>
             <Route path="users" element={<AdminUser />} />
             <Route path="contact" element={<AdminContact />} />
             <Route path="updateuser/:id" element={<UpdateUser />} />
-          </Route>
+          </Route> */}
         </Routes>
         <Footer />
       </BrowserRouter>
